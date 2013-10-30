7dd2cfb135b18003a0518ffdd060aa30ebe2a700
SBetzin <sebastian@betzin.com>
2013-10-30 09:46:53 +0100

    Inserted Unit Tests für AddOrMerge

---
+++ test/WindowsAzure.Tests/Table/Context/AddOrMergeEntitiesTests.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+using Moq;
+using WindowsAzure.Table;
+using WindowsAzure.Table.RequestExecutor;
+using WindowsAzure.Tests.Common;
+using WindowsAzure.Tests.Samples;
+using Xunit;
+
+namespace WindowsAzure.Tests.Table.Context
+{
+    public sealed class AddOrMergeEntitiesTests
+    {
+        [Fact]
+        public void AddOrMergeEntities()
+        {
+            // Arrange
+            Mock<ITableRequestExecutor<Country>> mock = MocksFactory.GetQueryExecutorMock<Country>();
+            CloudTableClient tableClient = ObjectsFactory.GetCloudTableClient();
+            var context = new TableSet<Country>(tableClient)
+                {
+                    RequestExecutor = mock.Object
+                };
+
+            var countries = ObjectsFactory.GetCountries();
+
+            // Act
+            var result = context.AddOrMerge(countries);
+
+            // Assert
+            Assert.NotNull(result);
+            mock.Verify(executor => executor.ExecuteBatches(countries, TableOperation.InsertOrMerge), Times.Once());
+            Assert.Equal(countries, result);
+        }
+
+        [Fact]
+        public void AddOrMergeEntitiesWithNullParameter()
+        {
+            // Arrange
+            Mock<ITableRequestExecutor<Country>> mock = MocksFactory.GetQueryExecutorMock<Country>();
+            CloudTableClient tableClient = ObjectsFactory.GetCloudTableClient();
+            var context = new TableSet<Country>(tableClient)
+                {
+                    RequestExecutor = mock.Object
+                };
+
+            IEnumerable<Country> result = null;
+
+            // Act
+            Assert.Throws<ArgumentNullException>(() => { result = context.AddOrMerge((IEnumerable<Country>)null); });
+
+            // Assert
+            Assert.Null(result);
+            mock.Verify(executor => executor.ExecuteBatches(It.IsAny<IEnumerable<Country>>(), It.IsAny<Func<ITableEntity, TableOperation>>()), Times.Never());
+        }
+
+        [Fact]
+        public void AddOrMergeEmptyCollection()
+        {
+            // Arrange
+            Mock<ITableRequestExecutor<Country>> mock = MocksFactory.GetQueryExecutorMock<Country>();
+            CloudTableClient tableClient = ObjectsFactory.GetCloudTableClient();
+            var context = new TableSet<Country>(tableClient)
+            {
+                RequestExecutor = mock.Object
+            };
+
+            var countries = new List<Country>();
+
+            // Act
+            var result = context.AddOrMerge(countries);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(countries, result);
+        }
+
+        [Fact]
+        public async Task AddOrMergeEntitiesAsync()
+        {
+            // Arrange
+            Mock<ITableRequestExecutor<Country>> mock = MocksFactory.GetQueryExecutorMock<Country>();
+            CloudTableClient tableClient = ObjectsFactory.GetCloudTableClient();
+            var context = new TableSet<Country>(tableClient)
+                {
+                    RequestExecutor = mock.Object
+                };
+
+            var countries = ObjectsFactory.GetCountries();
+
+            // Act
+            var result = await context.AddOrMergeAsync(countries);
+
+            // Assert
+            Assert.NotNull(result);
+            mock.Verify(executor => executor.ExecuteBatchesAsync(countries, TableOperation.InsertOrMerge, It.IsAny<CancellationToken>()));
+            Assert.Equal(countries, result);
+        }
+
+        [Fact]
+        public async Task AddOrMergeEntitiesWithNullParameterAsync()
+        {
+            // Arrange
+            Mock<ITableRequestExecutor<Country>> mock = MocksFactory.GetQueryExecutorMock<Country>();
+            CloudTableClient tableClient = ObjectsFactory.GetCloudTableClient();
+            var context = new TableSet<Country>(tableClient)
+                {
+                    RequestExecutor = mock.Object
+                };
+
+            IEnumerable<Country> result = null;
+
+            // Act
+            try
+            {
+                result = await context.AddOrMergeAsync((IEnumerable<Country>)null, CancellationToken.None);
+            }
+            catch (ArgumentNullException)
+            {
+            }
+
+            // Assert
+            Assert.Null(result);
+            mock.Verify(executor => executor.ExecuteBatches(It.IsAny<IEnumerable<Country>>(), It.IsAny<Func<ITableEntity, TableOperation>>()), Times.Never());
+        }
+
+        [Fact]
+        public async Task AddOrMergeEmptyCollectionAsync()
+        {
+            // Arrange
+            Mock<ITableRequestExecutor<Country>> mock = MocksFactory.GetQueryExecutorMock<Country>();
+            CloudTableClient tableClient = ObjectsFactory.GetCloudTableClient();
+            var context = new TableSet<Country>(tableClient)
+            {
+                RequestExecutor = mock.Object
+            };
+
+            var countries = new List<Country>();
+
+            // Act
+            var result = await context.AddOrMergeAsync(countries);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(countries, result);
+        }
+    }
+}
---
+++ test/WindowsAzure.Tests/Table/Context/AddOrMergeEntityTests.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+using Moq;
+using WindowsAzure.Table;
+using WindowsAzure.Table.RequestExecutor;
+using WindowsAzure.Tests.Common;
+using WindowsAzure.Tests.Samples;
+using Xunit;
+
+namespace WindowsAzure.Tests.Table.Context
+{
+    public sealed class AddOrMergeEntityTests
+    {
+        [Fact]
+        public void AddOrMergeEntity()
+        {
+            // Arrange
+            Mock<ITableRequestExecutor<Country>> mock = MocksFactory.GetQueryExecutorMock<Country>();
+            CloudTableClient tableClient = ObjectsFactory.GetCloudTableClient();
+            var context = new TableSet<Country>(tableClient)
+                {
+                    RequestExecutor = mock.Object
+                };
+
+            Country country = ObjectsFactory.GetCountry();
+
+            // Act
+            Country result = context.AddOrMerge(country);
+
+            // Assert
+            Assert.NotNull(result);
+            mock.Verify(executor => executor.Execute(country, TableOperation.InsertOrMerge), Times.Once());
+            Assert.Equal(country, result);
+        }
+
+        [Fact]
+        public void AddOrMergeEntityWithNullParameter()
+        {
+            // Arrange
+            Mock<ITableRequestExecutor<Country>> mock = MocksFactory.GetQueryExecutorMock<Country>();
+            CloudTableClient tableClient = ObjectsFactory.GetCloudTableClient();
+            var context = new TableSet<Country>(tableClient)
+                {
+                    RequestExecutor = mock.Object
+                };
+
+            Country result = null;
+
+            // Act
+            Assert.Throws<ArgumentNullException>(() => { result = context.AddOrMerge((Country) null); });
+
+            // Assert
+            Assert.Null(result);
+            mock.Verify(executor => executor.Execute(It.IsAny<Country>(), It.IsAny<Func<ITableEntity, TableOperation>>()), Times.Never());
+        }
+
+        [Fact]
+        public async Task AddOrMergeEntityAsync()
+        {
+            // Arrange
+            Mock<ITableRequestExecutor<Country>> mock = MocksFactory.GetQueryExecutorMock<Country>();
+            CloudTableClient tableClient = ObjectsFactory.GetCloudTableClient();
+            var context = new TableSet<Country>(tableClient)
+                {
+                    RequestExecutor = mock.Object
+                };
+
+            Country country = ObjectsFactory.GetCountry();
+
+            // Act
+            Country result = await context.AddOrMergeAsync(country);
+
+            // Assert
+            Assert.NotNull(result);
+            mock.Verify(executor => executor.ExecuteAsync(country, TableOperation.InsertOrMerge, It.IsAny<CancellationToken>()));
+            Assert.Equal(country, result);
+        }
+
+        [Fact]
+        public async Task AddOrMergeEntityWithNullParameterAsync()
+        {
+            // Arrange
+            Mock<ITableRequestExecutor<Country>> mock = MocksFactory.GetQueryExecutorMock<Country>();
+            CloudTableClient tableClient = ObjectsFactory.GetCloudTableClient();
+            var context = new TableSet<Country>(tableClient)
+                {
+                    RequestExecutor = mock.Object
+                };
+
+            Country result = null;
+
+            // Act
+            try
+            {
+                result = await context.AddOrMergeAsync((Country) null, CancellationToken.None);
+            }
+            catch (ArgumentNullException)
+            {
+            }
+
+            // Assert
+            Assert.Null(result);
+            mock.Verify(executor => executor.Execute(It.IsAny<Country>(), It.IsAny<Func<ITableEntity, TableOperation>>()), Times.Never());
+        }
+    }
+}
