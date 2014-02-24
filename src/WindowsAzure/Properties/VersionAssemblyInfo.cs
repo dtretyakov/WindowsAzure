022014ad821af8beab9967a98d66e2aaf7bdc487
Gabriel Marquez <gabriel.marquez@fepin.com.br>
2014-02-24 14:09:48 -0300

    BUILD: New version of WindowsAzure.StorageExtensions

--- src/WindowsAzure/Properties/VersionAssemblyInfo.cs
+++ src/WindowsAzure/Properties/VersionAssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.8.124")]
-[assembly: AssemblyFileVersion("0.8.124")]
-[assembly: AssemblyInformationalVersion("0.8.124")]
+[assembly: AssemblyVersion("0.8.126")]
+[assembly: AssemblyFileVersion("0.8.126")]
+[assembly: AssemblyInformationalVersion("0.8.126")]
