9024ffe3aec840a4a82edc506f736b1c62663984
Tom Dietrich <tom.dietrich@gmail.com>
2015-03-10 21:15:27 -0400

    PR review changes
    Removing Azure SDK namespace references from ITableSet interface

--- src/WindowsAzure/Table/ITableSet.cs
+++ src/WindowsAzure/Table/ITableSet.cs
@@ -2,8 +2,6 @@
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Table;
 
 namespace WindowsAzure.Table
 {
@@ -171,12 +169,10 @@ namespace WindowsAzure.Table
         /// <summary>
         ///    Creates the table if it does not already exist.
         /// </summary>      
-        /// <param name="requestOptions">A <see cref="T:Microsoft.WindowsAzure.Storage.Table.TableRequestOptions"/> object that specifies additional options for the request.</param>
-        /// <param name="operationContext">An <see cref="T:Microsoft.WindowsAzure.Storage.OperationContext"/> object that represents the context for the current operation.</param>
         /// <returns>
         /// <c>true</c> if table was created; otherwise, <c>false</c>.
         /// </returns>                  
-        void CreateIfNotExists(TableRequestOptions requestOptions = null, OperationContext operationContext = null );
+        void CreateIfNotExists();
 
         /// <summary>
         ///     Initiates an asynchronous operation to create a table if it does not already exist.        
@@ -194,24 +190,5 @@ namespace WindowsAzure.Table
         /// A <see cref="T:System.Threading.Tasks.Task`1"/> object of type <c>bool</c> that represents the asynchronous operation.
         /// </returns>
         Task<bool> CreateIfNotExistsAsync(CancellationToken cancellationToken);
-
-        /// <summary>
-        ///     Initiates an asynchronous operation to create a table if it does not already exist.        
-        /// </summary>
-        /// <param name="requestOptions">A <see cref="T:Microsoft.WindowsAzure.Storage.Table.TableRequestOptions"/> object that specifies additional options for the request.</param>
-        /// <param name="operationContext">An <see cref="T:Microsoft.WindowsAzure.Storage.OperationContext"/> object that represents the context for the current operation.</param>
-        /// <returns>
-        /// A <see cref="T:System.Threading.Tasks.Task`1"/> object of type <c>bool</c> that represents the asynchronous operation.
-        /// </returns>
-        Task<bool> CreateIfNotExistsAsync(TableRequestOptions requestOptions, OperationContext operationContext);
-
-        /// <summary>
-        ///     Initiates an asynchronous operation to create a table if it does not already exist.        
-        /// </summary>
-        /// <param name="requestOptions">A <see cref="T:Microsoft.WindowsAzure.Storage.Table.TableRequestOptions"/> object that specifies additional options for the request.</param><param name="operationContext">An <see cref="T:Microsoft.WindowsAzure.Storage.OperationContext"/> object that represents the context for the current operation.</param><param name="cancellationToken">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for a task to complete.</param>
-        /// <returns>
-        /// A <see cref="T:System.Threading.Tasks.Task`1"/> object of type <c>bool</c> that represents the asynchronous operation.
-        /// </returns>
-        Task<bool> CreateIfNotExistsAsync(TableRequestOptions requestOptions, OperationContext operationContext, CancellationToken cancellationToken);
     }
 }
